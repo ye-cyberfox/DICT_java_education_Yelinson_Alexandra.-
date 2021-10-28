2e37b0cdaac15b88ca57a6a6f789f674fba41cdf
Alexandra Yelinson 539 <a.o.yelinson@student.khai.edu>
2021-10-28 13:08:02 +0300

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,11 @@
+package ChatBot;
+import java.util.Scanner;
+
+public class ChatBot {
+    public static void main(String[] args){
+        //STAGE1
+        String bot_name = "Чонгук";
+        int birth_year = 2021;
+        System.out.println("Привет! Меня зовут "+bot_name+".\n"+"Я был создан в "+ birth_year+".");
+    }
+}
