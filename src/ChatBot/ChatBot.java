80fb156a545ea5412b063daf4b795a76beca9a7e
Alexandra Yelinson 539 <a.o.yelinson@student.khai.edu>
2021-10-28 19:39:02 +0300

    Chatbot 5-th stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -13,19 +13,31 @@ public class ChatBot {
         String user_name = scanner.nextLine();
         System.out.println("Я рад знакомству, "+ user_name+"!");
         //STAGE3
-        System.out.println("Я могу угадать сколько тебе лет.\nТолько напиши остаток от деления твоего возраста на 3, 5 и 7");
+        System.out.println("Я могу угадать твой возраст.\nТолько напиши остаток от деления твоего возраста на 3, 5 и 7");
         int remainder3 = scanner.nextInt();
         int remainder5 = scanner.nextInt();
         int remainder7 = scanner.nextInt();
         int age = (remainder3*70+remainder5*21+remainder7*15)%105;
-        System.out.println("Я думаю, что тебе "+age+" лет :) Круто быть умным, как я!");
+        System.out.println("Я думаю, что тебе "+age+":) Круто быть умным, как я!");
         //STAGE4
         System.out.println("Ещё я хорошо считаю. Введи число и я легко посчитаю до него.");
         int number = scanner.nextInt();
         for (int i=0; i<number+1; i++){
             System.out.println(i+"!");
+            if (i==number){
+            System.out.println("Готово!");}
         }
-
-
+        //STAGE5
+        System.out.println("Давай теперь проверим твою внимательность. Я задам вопрос, а ты выбери правильный ответ (цифру).\nВопроc: как меня зовут?");
+        System.out.println("1.Глеб\n2.Чонгук\n3.Cерафим\n4.Алекс");
+        while (true) {
+        int user_answer = scanner.nextInt();
+        if(user_answer==2){
+            System.out.println("Верно ты запомнил! Я тоже помню твое имя, "+user_name+"!\nЕще обязательно увидимся! Хорошего тебе дня:)");
+            break;}
+        else {
+            System.out.println("Неет:( Дам тебе еще попытку!");}
+            }
+        }
     }
-}
+
