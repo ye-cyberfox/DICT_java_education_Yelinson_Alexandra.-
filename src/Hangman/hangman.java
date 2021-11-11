c719a0931d8170cc1fd8e614e1f1eb2a1b5495ae
Alexandra Yelinson 539 <a.o.yelinson@student.khai.edu>
2021-11-11 21:20:17 +0200

    2-nd stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -5,5 +5,15 @@ public class hangman {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Висилица!\nИгра скоро будет доступна...");
+        //STAGE2
+        System.out.print("Угадай слово: ");
+        String answer = scan.nextLine();
+        String word = "java";
+        if (answer.equals(word)){
+            System.out.println("Вы выжили!");
+        }
+        else{
+            System.out.println("Вы проиграли!");
+        }
     }
 }
