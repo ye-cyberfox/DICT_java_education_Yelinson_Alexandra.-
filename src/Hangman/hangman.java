68c0a1178436cc638ec0a49291ee2366298659a1
Alexandra Yelinson 539 <a.o.yelinson@student.khai.edu>
2021-11-11 23:19:51 +0200

    4-th stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -9,24 +9,36 @@ public class hangman {
         System.out.print("Угадай слово: ");
         String answer = scan.nextLine();
         String word = "java";
-        if (answer.equals(word)){
+        if (answer.equals(word)) {
             System.out.println("Вы выжили!");
-        }
-        else{
+        } else {
             System.out.println("Вы проиграли!");
         }
         //STAGE3
         Random random = new Random();
-        String[] words = {"java","python", "javascript", "kotlin"};
+        String[] words = {"java", "python", "javascript", "kotlin"};
         String variation = words[random.nextInt(words.length)];
         System.out.print("Раунд 2\nУгадай слово: ");
         String answer2 = scan.nextLine();
-        if (answer2.equals(variation)){
+        if (answer2.equals(variation)) {
             System.out.println("Вы выжили!");
-        }
-        else{
+        } else {
             System.out.println("Вы проиграли!");
         }
-
+        //STAGE4
+        char[] symbol = variation.toCharArray();
+        for (int i = 2; i < symbol.length; i++) {
+            symbol[i] = '-';
+        }
+        String secret = String.valueOf(symbol);
+        System.out.print("Раунд 3\nУгадай слово "+secret+ ": ");
+        String answer3 = scan.nextLine();
+        if (variation.equals(answer3)) {
+            System.out.println("Вы выжили!");
+        } else {
+            System.out.println("Вы проиграли!");
+        }
     }
 }
+
+
