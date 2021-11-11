8eb21a1d60499c4e9402d0f4f4ab0ffd00fb153e
Alexandra Yelinson 539 <a.o.yelinson@student.khai.edu>
2021-11-11 20:20:04 +0200

    1-st stage

---
+++ src/Hangman/hangman.java
@@ -0,0 +1,9 @@
+package Hangman;
+import java.util.*;
+
+public class hangman {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Висилица!\nИгра скоро будет доступна...");
+    }
+}
