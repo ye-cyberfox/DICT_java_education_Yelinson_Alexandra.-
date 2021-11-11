8c0b6f30e8297681ff81c3bbe2e2bb3bf5d1d1a8
Alexandra Yelinson 539 <a.o.yelinson@student.khai.edu>
2021-11-11 21:51:56 +0200

    3-rd stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -15,5 +15,18 @@ public class hangman {
         else{
             System.out.println("Вы проиграли!");
         }
+        //STAGE3
+        Random random = new Random();
+        String[] words = {"java","python", "javascript", "kotlin"};
+        String variation = words[random.nextInt(words.length)];
+        System.out.print("Раунд 2\nУгадай слово: ");
+        String answer2 = scan.nextLine();
+        if (answer2.equals(variation)){
+            System.out.println("Вы выжили!");
+        }
+        else{
+            System.out.println("Вы проиграли!");
+        }
+
     }
 }
