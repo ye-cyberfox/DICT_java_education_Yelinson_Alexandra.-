12cdd5b1988251eb36f12edd97c359886be69144
Alexandra Yelinson 539 <a.o.yelinson@student.khai.edu>
2021-11-13 21:56:11 +0200

    5-th stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -4,39 +4,42 @@ import java.util.*;
 public class hangman {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Висилица!\nИгра скоро будет доступна...");
-        //STAGE2
-        System.out.print("Угадай слово: ");
-        String answer = scan.nextLine();
-        String word = "java";
-        if (answer.equals(word)) {
-            System.out.println("Вы выжили!");
-        } else {
-            System.out.println("Вы проиграли!");
-        }
-        //STAGE3
         Random random = new Random();
+        System.out.println("Висилица!\nИгра скоро будет доступна...");
+
+        ArrayList<String> someLetters = new ArrayList<>();
         String[] words = {"java", "python", "javascript", "kotlin"};
         String variation = words[random.nextInt(words.length)];
-        System.out.print("Раунд 2\nУгадай слово: ");
-        String answer2 = scan.nextLine();
-        if (answer2.equals(variation)) {
-            System.out.println("Вы выжили!");
-        } else {
-            System.out.println("Вы проиграли!");
-        }
-        //STAGE4
+        StringBuffer secret = new StringBuffer(variation);
         char[] symbol = variation.toCharArray();
-        for (int i = 2; i < symbol.length; i++) {
-            symbol[i] = '-';
+        for (int i = 0; i < symbol.length; i++) {
+            secret.setCharAt(i,'-');
         }
-        String secret = String.valueOf(symbol);
-        System.out.print("Раунд 3\nУгадай слово "+secret+ ": ");
-        String answer3 = scan.nextLine();
-        if (variation.equals(answer3)) {
-            System.out.println("Вы выжили!");
-        } else {
-            System.out.println("Вы проиграли!");
+        short health = 8;
+        boolean True = true;
+        while (True) {
+            if (health > 0) {
+                System.out.print(secret + "\nВведите букву:");
+                String user = scan.nextLine();
+                someLetters.add(user);
+                health--;
+                if (variation.contains(user)) {
+                    for (int i = 0; i < variation.length(); i++) {
+                        if (symbol[i] == user.charAt(0)) {
+                            secret.setCharAt(i, user.charAt(0));
+                        }
+                    }
+                    if (secret.toString().equals(variation)) {
+                        System.out.println(variation+"\nПоздравляю с победой!\nУвидимся на следующем этапе!");
+                        break;
+                    }
+                } else {
+                    System.out.println("Этой буквы нет в слове!");
+                }
+            } else {
+                System.out.println("Вы проиграли!");
+                break;
+            }
         }
     }
 }
