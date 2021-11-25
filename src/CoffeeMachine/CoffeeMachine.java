404ab7f7e699d852a61fb6420e6d82bc6a81f793
Alexandra Yelinson 539 <a.o.yelinson@student.khai.edu>
2021-11-25 21:07:34 +0200

    3-rd stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,5 +1,6 @@
 package CoffeeMachine;
-import java.util.Scanner;
+import java.util.*;
+
 
 public class CoffeeMachine {
     public static void main(String[] args){
@@ -9,8 +10,21 @@ public class CoffeeMachine {
         int beans= 15;
         System.out.println("Write how many cups of coffee you will need:");
         int cup = scan.nextInt();
-        System.out.println("For " + cup + " cups of coffee you will need:\n"+ cup * water +" ml of water\n" +
-                cup * milk + " ml of milk\n" +
-                cup * beans + " g of coffee beans" );
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int water_machine = scan.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milk_machine = scan.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int beans_machine = scan.nextInt();
+        int[] array= new int[] {water_machine / water, milk_machine / milk, beans_machine / beans};
+        var min_cup = Arrays.stream(array).min().getAsInt();
+
+        if (min_cup == cup){
+            System.out.println("Yes, I can make that amount of coffee");
+        } if (min_cup > cup) {
+            System.out.println("Yes, I can make that amount of coffee (and even " + (min_cup - cup) + " more than that)");
+        } else {
+            System.out.println("No, I can make only " + min_cup + " cups of coffee");
+        }
     }
 }
