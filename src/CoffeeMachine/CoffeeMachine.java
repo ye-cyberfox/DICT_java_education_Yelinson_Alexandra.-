31a81bed5b62439a88226adbc70b4ff94200af3d
Alexandra Yelinson 539 <a.o.yelinson@student.khai.edu>
2021-11-26 12:23:46 +0200

    5-th stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -12,30 +12,52 @@ public class CoffeeMachine {
         System.out.println("The coffee machine has:\n" +
                 water + "ml of water\n" + milk + "ml of milk\n" +
                 beans + "g of coffee beans\n" + paper_cup + " of paper cups\n" + money + "₴ of money");
-        System.out.println("\nWrite action (buy, fill, take):");
-        String user = scan.nextLine();
+        while (true) {
+            System.out.println("\nWrite action (buy, fill, take, remaining, exit):");
+            String user = scan.nextLine();
             if (user.equals("buy")) {
-                System.out.println("Choose your coffee: (1 - Espresso, 2 - Latte, 3 - Cappuccino) ");
-                int coffee = scan.nextInt();
-                if (coffee == 1) {
-                    water -= 250;
-                    beans -= 16;
-                    money += 4;
-                    paper_cup--;
-                } else if (coffee == 2) {
-                    water -= 350;
-                    milk -= 75;
-                    beans -= 20;
-                    money += 7;
-                    paper_cup--;
-                } else if (coffee == 3) {
-                    water -= 200;
-                    milk -= 100;
-                    beans -= 12;
-                    money += 6;
-                    paper_cup--;
+                while (true) {
+                    System.out.println("Choose your coffee: (1 - Espresso, 2 - Latte, 3 - Cappuccino, 4 - back to main menu>) ");
+                    int coffee = scan.nextInt();
+                    if (coffee == 1) {
+                        if (water < 250  || beans < 16 || paper_cup < 1) {
+                            System.out.println("Sorry, not enough ingredients!");
+                        } else {
+                            water -= 250;
+                            beans -= 16;
+                            money += 4;
+                            paper_cup--;
+                            System.out.println("I have enough resources, making you a coffee!");
+                        }
+                    }
+                    if (coffee == 2) {
+                        if (water < 350 || milk < 75 || beans < 20 || paper_cup < 1) {
+                            System.out.println("Sorry, not enough ingredients!");
+                        } else {
+                            water -= 350;
+                            milk -= 75;
+                            beans -= 20;
+                            money += 7;
+                            paper_cup--;
+                            System.out.println("I have enough resources, making you a coffee!");
+                        }
+                    }
+                    if (coffee == 3) {
+                        if (water < 200 || milk < 100 || beans < 12 || paper_cup < 1) {
+                            System.out.println("Sorry, not enough ingredients!");
+                        } else {
+                            water -= 200;
+                            milk -= 100;
+                            beans -= 12;
+                            money += 6;
+                            paper_cup--;
+                            System.out.println("I have enough resources, making you a coffee!");
+                        }
+                    } if(coffee == 4){
+                        break;
+                    }
                 }
-            } else if (user.equals("fill")) {
+            }if (user.equals("fill")) {
                 System.out.println("Write how many ml of water you want to add: ");
                 water += scan.nextInt();
                 System.out.println("Write how many ml of milk you want to add: ");
@@ -44,12 +66,17 @@ public class CoffeeMachine {
                 beans += scan.nextInt();
                 System.out.println("Write how many paper_cup you want to add: ");
                 paper_cup += scan.nextInt();
-            }
-            else if (user.equals("take")){
-                System.out.println("I gave you "+ money +"₴");
+            } if (user.equals("take")) {
+                System.out.println("I gave you " + money + "₴");
                 money -= money;
+            } if (user.equals("remaining")){
+                System.out.println("\nThe coffee machine has:\n" + water + "ml of water\n" + milk +"ml of milk\n"
+                        + beans + "g of coffee beans\n" + paper_cup + " of paper cups\n" + money + "₴ of money");
+            } if (user.equals("exit")) {
+                break;
             }
-        System.out.println("\nThe coffee machine has:\n" + water + "ml of water\n" +milk +
-                "ml of milk\n" +beans + "g of coffee beans\n" +paper_cup + " of paper cups\n" +money + "₴ of money");
+        }
+
     }
 }
+
