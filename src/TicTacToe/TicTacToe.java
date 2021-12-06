f7b0bc79953bde244628f6311023a97d9ff16174
Alexandra Yelinson 539 <a.o.yelinson@student.khai.edu>
2021-12-06 12:57:53 +0200

    1-st stage

---
+++ src/TicTacToe/TicTacToe.java
@@ -0,0 +1,10 @@
+package TicTacToe;
+
+public class TicTacToe {
+    public static void main(String[] args){
+        System.out.println("X O X\n" +
+                "O X O\n" +
+                "X X O");
+    }
+}
+
