bad0e6c3479984fc81bd05af7061fe24a6a13762
Alexandra Yelinson 539 <a.o.yelinson@student.khai.edu>
2021-12-10 22:24:51 +0200

    stage 1

---
+++ src/CreditCalculator/CreditCalculator.java
@@ -0,0 +1,14 @@
+package CreditCalculator;
+
+public class CreditCalculator {
+    public static void main(String[] args) {
+        System.out.println("""
+                Loan principal: 1000
+                Month 1:repaid 250
+                Month 2:repaid 250
+                Month 3:repaid 500
+                The loan has been repaid
+                """);
+    }
+}
+
