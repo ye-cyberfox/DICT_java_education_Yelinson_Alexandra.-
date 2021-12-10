8ea53f8a7badb20f426415a01cf771a520d11ea8
Alexandra Yelinson 539 <a.o.yelinson@student.khai.edu>
2021-12-10 23:42:02 +0200

    stage 3

--- src/CreditCalculator/CreditCalculator.java
+++ src/CreditCalculator/CreditCalculator.java
@@ -3,47 +3,63 @@ package CreditCalculator;
 import java.util.Scanner;
 
 public class CreditCalculator {
-    public static int payment,loan,months;
+    static double loan,loanPrincipal,monthNumber,paymentMonth,i;
     public static final Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("""
-                Loan principal: 1000
-                Month 1:repaid 250
-                Month 2:repaid 250
-                Month 3:repaid 500
-                The loan has been repaid
-                """);
-        System.out.println("Enter the loan principal");
-        loan = in.nextInt();
-        System.out.println("""
                 What do you want to calculate?
-                type "m" - for number of monthly payments,
-                type "p" - for the monthly payment:""");
+                type "n" - for number of monthly payments,
+                type "a" - for annuity monthly payment amount,
+                type "p" - for the loan principal:""");
         String user = in.next();
-        if (user.equals("m")) {
+        if (user.equals("n")) {
+            System.out.println("Enter a loan principal:");
+            loanPrincipal = in.nextInt();
             System.out.println("Enter the monthly payment:");
-            int monthPay = in.nextInt();
-            if (loan % monthPay ==0){
-                months = loan / monthPay;
-            } else {
-                months = loan / monthPay +1;
-            }
-            System.out.println("It will take " + months + " month to repay the loan.");
+            paymentMonth = in.nextInt();
+            loanInterest();
+            monthNumber = Math.log(paymentMonth/(paymentMonth-(i*loanPrincipal)))/Math.log(1+i);
+            period();
+        }
+        if (user.equals("a")) {
+            System.out.println("Enter a loan principal:");
+            loanPrincipal = in.nextDouble();
+            System.out.println("Enter the number of periods:");
+            monthNumber= in.nextInt();
+            loanInterest();
+            paymentMonth = loanPrincipal*((i*Math.pow((1+i),monthNumber))/(Math.pow((1+i),monthNumber)-1));
+            System.out.printf("Your monthly payment = %.0f", paymentMonth);
         }
         if (user.equals("p")) {
-            System.out.println("Enter number of month:");
-            months = in.nextInt();
-            if (loan % months ==0){
-                payment = loan / months;
-                System.out.println("Your monthly payment = " + payment);
-            }
-            else {
-                payment = loan / months + 1;
-                int lastPayment = loan-(months-1)*payment;
-                System.out.println("Your monthly payment = " + payment + " and the last payment = " + lastPayment);
-                }
-            }
+            System.out.println("Enter the annuity payment:");
+            paymentMonth = in.nextDouble();
+            System.out.println("Enter the number of periods:");
+            monthNumber= in.nextInt();
+            loanInterest();
+            loanPrincipal=paymentMonth/((i*Math.pow((1+i),monthNumber))/(Math.pow((1+i),monthNumber)-1));
+            System.out.printf("Your loan principal = %.0f", loanPrincipal);
         }
     }
+    public static void loanInterest(){
+        System.out.println("Enter a loan interest:");
+        loan = in.nextDouble();
+        i =  (loan/(12*100));
+    }
+    public static void period(){
+        int period = (int) Math.round(monthNumber);
+        int year = (period / 12);
+        int month = (period % 12);
+
+        if (year>0 & month >0){
+            System.out.println("It will take " + year + " years and " + month + " month to repay this loan");
+        } else if(year == 0){
+            System.out.println("It will take " + month + " month to repay this loan");
+        } else {
+            System.out.println("It will take " + year + " years to repay this loan");
+        }
+    }
+}
+
+
 
